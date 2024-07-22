37f6c652a384e2eedacb029b7323261a286959cd
OneCloudd <whdlfdns12@gmail.com>
2024-07-22 17:45:34 +0900

    first commit

---
+++ src/main/java/com/github/project2/config/JpaConfig.java
@@ -0,0 +1,60 @@
+package com.github.project2.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = {""},
+        entityManagerFactoryRef = "entityManagerFactoryBean1"
+)
+public class JpaConfig {
+
+    @Autowired
+    private DataSourceConfig dataSourceConfig;
+
+
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean1() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("com.github.project1.Entity.comment",
+                "com.github.project1.Entity.post",
+                "com.github.project1.Entity.user"
+        );
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.format_sql", "true");
+        properties.put("hibernate.use_sql_comment", "true");
+
+        em.setJpaPropertyMap(properties);
+
+        return em;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(dataSourceConfig.getDriverClassName());
+        dataSource.setUrl(dataSourceConfig.getUrl());
+        dataSource.setUsername(dataSourceConfig.getUsername());
+        dataSource.setPassword(dataSourceConfig.getPassword());
+
+        return dataSource;
+    }
+}
