a95bb1efd94f16342fbe7b1f9b6c4397b395de3d
junyoung22 <126755514+junyoung22@users.noreply.github.com>
2024-07-27 21:46:33 +0900

    category_product

--- src/main/java/com/github/project2/dto/post/ProductCategoryDto.java
+++ src/main/java/com/github/project2/dto/post/ProductCategoryDto.java
@@ -1,26 +1,28 @@
-//package com.github.project2.dto.post;
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import java.math.BigDecimal;
-//import java.time.LocalDate;
-//import java.time.LocalDateTime;
-//import java.util.List;
-//
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class ProductCategoryDto {
-//    private Integer id;
-//    private List<String> imageUrls;
-//    private String name;
-//    private BigDecimal price;
-//    private Integer stock;
-//    private LocalDate endDate;
-//    private String category;
-//}
+package com.github.project2.dto.post;
+
+import com.github.project2.entity.post.ProductCategoryEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductCategoryDto {
+    private Integer id;
+    private List<String> imageUrls;
+    private Integer category_id;
+    private String name;
+    private BigDecimal price;
+    private Integer stock;
+    private LocalDate endDate;
+    private String category;
+}
